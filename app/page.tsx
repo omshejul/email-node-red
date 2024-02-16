e9b0621b75b5bdaa3268d021e06aec66888f6e22
Om Shejul <contact@omshejul.com>
2024-02-16 23:22:10 +0530

    Update page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Form from "./Components/Form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center  lg:p-24">
       {/* <Form /> */}
-      <p className="grid place-items-center border p-10 rounded-xl" >Server Closed 🙅‍♂️</p>
+      <p className="grid place-items-center border p-4 lg:p-10 rounded-xl" >Server Closed 🙅‍♂️</p>
     </main>
   );
 }
